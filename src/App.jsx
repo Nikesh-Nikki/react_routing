095dbba1650106948773e2cb392b7d7af753c0e1
nikesh <nikesh.tadela@gmail.com>
2024-02-09 00:09:46 +0530

    added content to app and profile .jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 function App(){
-    return <>
-        <h1>Heyyy NikkiBucky!!</h1>
-        <div className="yoo"><p>lol</p></div>
-    </>
+    return (
+        <div>
+          <h1>Hello from the main page of the app!</h1>
+          <p>Here are some examples of links to other pages</p>
+          <nav>
+            <ul>
+              <li>
+                <a href="profile">Profile page</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      );
 }
 
 export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile(){
+    return (
+        <div>
+          <h1>Hello from profile page!</h1>
+          <p>So, how are you?</p>
+        </div>
+      );
+}
+
+export default Profile;
